ef495d97678081f0d51ee15346cc04e3c702e242
sparta-prog <debarun285@gmail.com>
2025-05-03 19:25:43 +0530

    Added kafka for notification service

---
+++ notification-service/src/main/java/com/programmingtechie/NotificationServiceApplication.java
@@ -0,0 +1,20 @@
+package com.programmingtechie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@SpringBootApplication
+@Slf4j
+public class NotificationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+    @KafkaListener(topics = "notificationTopic")
+    public void handleNotification(OrderPlacedEvent orderPlacedEvent) {
+        // Logic to send notification
+        log.info("Received Notification for Order Number: {}", orderPlacedEvent.getOrderNumber());
+    }
+}
--- order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
@@ -3,10 +3,12 @@ package com.programmingtechie.orderservice.service;
 import com.programmingtechie.orderservice.dto.InventoryResponse;
 import com.programmingtechie.orderservice.dto.OrderLineItemsDto;
 import com.programmingtechie.orderservice.dto.OrderRequest;
+import com.programmingtechie.orderservice.event.OrderPlacedEvent;
 import com.programmingtechie.orderservice.model.Order;
 import com.programmingtechie.orderservice.model.OrderLineItems;
 import com.programmingtechie.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -22,6 +24,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -50,11 +53,16 @@ public class OrderService {
 
             boolean allProductsInStock = Arrays.stream(inventoryResponses).allMatch(InventoryResponse::isInStock);
 
-            if (!allProductsInStock) {
+            if (allProductsInStock) {
+                orderRepository.save(order);
+                kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
+                return "Order Placed Successfully";
+            }
+            else {
+                //send message to the queue
                 throw new IllegalArgumentException("One or more items in the order are not available in inventory.");
             }
-            orderRepository.save(order);
-            return "Order Placed Successfully";
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
